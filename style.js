ba26bbefcac97a6c69e2fe9ec90c9e7feb76b24d
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-10 23:14:55 +0100

    nothing to commit

--- style.js
+++ style.js
@@ -1,4 +1,6 @@
-const movie = "https://api.audiomack.com/v1"
-const getMovie = () =>{
-    console.log(movie)
+const movie = "https://vpic.nhtsa.dot.gov/api/"
+const getMovie = async() =>{
+    let response = await fetch(movie)
+    let jsonResponse = await response.json()
+    console.log(jsonResponse)
 }
