e5f0c1ea318e17de990346bdaa65e953076b5ebc
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-29 13:23:25 +0100

    displayed content

--- style.js
+++ style.js
@@ -12,9 +12,10 @@ const getMovie =  async() =>{
                 <h1>Lifespan:${animalInfo[0].lifespan}</h1>
                 <h1>Weight:${animalInfo[0].weight_max}</h1>
                 <h1>Diet:${animalInfo[0].diet}</h1>
+                <img class = "w-50" src="${animalInfo[0].image_link}">
                 `   
-           image.innerHTML = `
-           <img src="${animalInfo[0].image_link}">`       
+        //    image.innerHTML = `
+        //    <img class = "w-50" src="${animalInfo[0].image_link}">`       
 }
 
 
@@ -32,13 +33,17 @@ const getMovie =  async() =>{
 // const movie = "https://zoo-animal-api.herokuapp.com/animals/rand"
 // const movie = "https://www.fishwatch.gov/api/species"
 // const getMovie =  async() =>{
+//     let animalInfo = []
 //     let response = await fetch(movie)
-//     let dataInJson = await response.json()
-//     // console.log(jsonResponse)
-//         // disp.innerHTML =`
-//         // <h1>${.category}</h1>
-//         // `
-//         jsonResponse.map((_,i)=>{
-//             let habitat = dataInJson[i].habitat
-//         })     
+//     let jsonResponse = await response.json()
+//         console.log(jsonResponse)
+//         animalInfo.push(jsonResponse)
+//         disp.innerHTML =`
+//                 <h1>Name:${animalInfo[0][SpeciesName]}</h1>
+//                 <h1>Latin name:${animalInfo[0].latin_name}</h1>
+//                 <h1>Habitat:${animalInfo[0].habitat}</h1>
+//                 <h1>Lifespan:${animalInfo[0].lifespan}</h1>
+//                 <h1>Weight:${animalInfo[0].weight_max}</h1>
+//                 <h1>Diet:${animalInfo[0].diet}</h1>
+//                 `      
 // }
