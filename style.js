bc1ccceb79353fb11ebb2a9891f3d73afe4323be
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-29 12:45:21 +0100

    added api

--- style.js
+++ style.js
@@ -1,9 +1,42 @@
-const movie = "https://opentdb.com/api.php?amount=20&difficulty=medium"
+const movie = "https://zoo-animal-api.herokuapp.com/animals/rand"
 const getMovie =  async() =>{
+    let animalInfo = []
     let response = await fetch(movie)
     let jsonResponse = await response.json()
     console.log(jsonResponse)
-        // disp.innerHTML =`
-        // <h1>${.category}</h1>
-        // `     
+    animalInfo.push(jsonResponse)
+        disp.innerHTML =`
+                <h1>Name:${animalInfo[0].name}</h1>
+                <h1>Latin name:${animalInfo[0].latin_name}</h1>
+                <h1>Habitat:${animalInfo[0].habitat}</h1>
+                <h1>Lifespan:${animalInfo[0].lifespan}</h1>
+                <h1>Weight:${animalInfo[0].weight_max}</h1>
+                <h1>Diet:${animalInfo[0].diet}</h1>
+                `     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const movie = "https://zoo-animal-api.herokuapp.com/animals/rand"
+// const movie = "https://www.fishwatch.gov/api/species"
+// const getMovie =  async() =>{
+//     let response = await fetch(movie)
+//     let dataInJson = await response.json()
+//     // console.log(jsonResponse)
+//         // disp.innerHTML =`
+//         // <h1>${.category}</h1>
+//         // `
+//         jsonResponse.map((_,i)=>{
+//             let habitat = dataInJson[i].habitat
+//         })     
+// }
