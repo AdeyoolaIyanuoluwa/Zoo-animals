ab20de772aea98f11bc1cbc498da3cecfadb3acd
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-29 13:51:04 +0100

    added heading

--- style.js
+++ style.js
@@ -6,16 +6,16 @@ const getMovie =  async() =>{
     console.log(jsonResponse)
     animalInfo.push(jsonResponse)
         disp.innerHTML =`
-                <h2>Name:${animalInfo[0].name}</h2>
-                <h1>Latin name:${animalInfo[0].latin_name}</h1>
-                <h1>Habitat:${animalInfo[0].habitat}</h1>
-                <h1>Lifespan:${animalInfo[0].lifespan}</h1>
-                <h1>Weight:${animalInfo[0].weight_max}</h1>
-                <h1>Diet:${animalInfo[0].diet}</h1>
+                <p>Name:${animalInfo[0].name}</p>
+                <p>Latin name:${animalInfo[0].latin_name}</p>
+                <p>Habitat:${animalInfo[0].habitat}</p>
+                <p>Lifespan:${animalInfo[0].lifespan}</p>
+                <p>Weight:${animalInfo[0].weight_max}</p>
+                <p>Diet:${animalInfo[0].diet}</p>
                 
                 `   
            image.innerHTML = `
-           <img class = "w-50" src="${animalInfo[0].image_link}">`       
+           <img class = "w-75" src="${animalInfo[0].image_link}">`       
 }
 
 
