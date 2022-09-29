c9add7de700033657e8c5151650452d8baf3a67d
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-29 12:14:45 +0100

    added html and js files

--- style.js
+++ style.js
@@ -1,6 +1,9 @@
-const movie = "https://vpic.nhtsa.dot.gov/api/"
-const getMovie = async() =>{
+const movie = "https://opentdb.com/api.php?amount=20&difficulty=medium"
+const getMovie =  async() =>{
     let response = await fetch(movie)
     let jsonResponse = await response.json()
     console.log(jsonResponse)
+        // disp.innerHTML =`
+        // <h1>${.category}</h1>
+        // `     
 }
